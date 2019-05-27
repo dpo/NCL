56e9b24536d315489a7bd4fd96b8407401f13c5f
Pierre-Elie <personnaz.pe@gmail.com>
2019-05-27 12:17:37 -0400

    debug ok ! contraintes

--- src/NCLModel.jl
+++ src/NCLModel.jl
@@ -15,7 +15,7 @@ mutable struct NCLModel_ <: AbstractNLPModel
 	y::AbstractVector
 	ρ::Number
 
-	# Informations of the original problem
+	# Information about the original problem
 	nvar_x::Int64
 	nvar_r::Int64
 	nlp_f_obj::Function
@@ -24,9 +24,11 @@ mutable struct NCLModel_ <: AbstractNLPModel
 	nlp_hess_lag::Function
 	nlp_hess_coord::Function
 	nlp_hprod::Function
+	minimize::Bool
+	standard::Bool
 end
 
-function NCLModel_(nlp::ADNLPModel, mult::AbstractVector, penal::Number)::NCLModel_
+function NCLModel_(nlp::ADNLPModel, mult::AbstractVector, penal::Number, nlp_standard::Bool)::NCLModel_
 	nvar = nlp.meta.nvar + nlp.meta.ncon - size(nlp.meta.lin, 1)
 	
 	# Parameters
@@ -34,18 +36,22 @@ function NCLModel_(nlp::ADNLPModel, mult::AbstractVector, penal::Number)::NCLMod
 	ρ = penal
 
 	# Previous model
-	nlp_f_obj(x), ∇nlp_f_obj(x) = objgrad(nlp, x)
+	
 	nlp_cons(x) = cons(nlp, x)
-	nlp_hess_lag(x, obj_weight, y) = hess(nlp, x; obj_weight, y)
-	nlp_hess_coord(x, obj_weight, y) = hess_coord(nlp, x; obj_weight, y)
-	nlp_hprod(nlp, x, v; obj_weight, y) = hprod(nlp, x, v; obj_weight, y)
+	nlp_f_obj(x) = obj(nlp, x)
+	∇nlp_f_obj(x) = grad(nlp, x)
+	nlp_hess_lag(x, obj_weight, y) = hess(nlp, x ; obj_weight = obj_weight, y = y)
+	nlp_hess_coord(x, obj_weight, y) = hess_coord(nlp, x ; obj_weight = obj_weight, y = y)
+	nlp_hprod(nlp, x, v, obj_weight, y) = hprod(nlp, x, v ; obj_weight = obj_weight, y = y)
 	nvar_x = nlp.meta.nvar
 	nvar_r = nlp.meta.ncon - size(nlp.meta.lin, 1) # linear constraints are not considered here in the NCL method. 
+	minimize = nlp.meta.minimize
+	standard = nlp_standard
 	# TODO: another implementation, considering linear constraints in the residues, as well
 
 
 	# Meta field
-	meta = NLPModelMeta(nlp.meta.nvar + nlp.meta.ncon - size(nlp.meta.lin, 1), ;
+	meta = NLPModelMeta(nlp.meta.nvar + nvar_r ;
 						lvar = vcat(nlp.meta.lvar, -Inf * ones(nvar_r)), # No bounds upon residues
 						uvar = vcat(nlp.meta.uvar, -Inf * ones(nvar_r)),
 						ncon = nlp.meta.ncon, 
@@ -67,16 +73,26 @@ function NCLModel_(nlp::ADNLPModel, mult::AbstractVector, penal::Number)::NCLMod
 					∇nlp_f_obj, 
 					nlp_cons, 
 					nlp_hess_lag, 
-					nlp_hprod)
+					nlp_hess_coord,
+					nlp_hprod,
+					minimize,
+					standard)
 end
 
 
 function NLPModels.obj(nlc::NCLModel_, z::AbstractVector)
 	increment!(ncl, :neval_obj)
-
-	return nlc.nlp_f_obj(z[1:nlc.nvar_x]) +
-		   nlc.y' * z[nlc.nvar_x + 1 : end] +
-		   0.5 * nlc.ρ * (norm(z[nlc.nvar_x + 1 : end], 2))
+	if nlc.minimize
+		return nlc.nlp_f_obj(z[1:nlc.nvar_x]) +
+		       nlc.y' * z[nlc.nvar_x + 1 : end] +
+			   0.5 * nlc.ρ * (norm(z[nlc.nvar_x + 1 : end], 2))
+	
+	else # argmax f(x) = argmin -f(x)
+		nlc.minimize = true 
+		return - nlc.nlp_f_obj(z[1:nlc.nvar_x]) +
+			   nlc.y' * z[nlc.nvar_x + 1 : end] +
+			   0.5 * nlc.ρ * (norm(z[nlc.nvar_x + 1 : end], 2))
+	end
 end
 
 
@@ -93,23 +109,16 @@ function NLPModels.grad!(nlc::NCLModel_, z::AbstractVector, gx::AbstractVector)
 end
 
 
-function NLPModels.hess(nlc::NCLModel_, z::AbstractVector; obj_weight=1.0, y::AbstractVector)
+function NLPModels.hess(nlc::NCLModel_, z::AbstractVector ; obj_weight=1.0, y=zeros)
 	increment!(nlc, :neval_hess)
-	H = zeros(nlc.nvar, nlc.nvar)
+	H = zeros(nlc.nvar, nlc.nvar) # = nvar_x + nvar_r -
 	H[1:nlc.nvar_x, 1:nlc.nvar_x] = nlc.nlp_hess_lag(z[1:nlc.nvar_x], obj_weight, y) # Original hessian
-	H[nlc.nvar_x+1:end, nlc.nvar_x+1:end] = nlc.ρ * identity(nlc.nvar_r) # Added by residues (constant because of quadratic penalization)
+	H[nlc.nvar_x+1:end, nlc.nvar_x+1:end] = H[nlc.nvar_x+1:end, nlc.nvar_x+1:end] + nlc.ρ * I # Added by residues (constant because of quadratic penalization) 
+	# TODO: check there is no problem with the I::Uniformscaling operator (size...)
 	return H
 end
 
-function NLPModels.hess!(nlc::NCLModel_, z::AbstractVector; obj_weight=1.0, y::AbstractVector, H::AbstractVector)
-	increment!(nlc, :neval_hess)
-	H[1:nlc.nvar_x, 1:nlc.nvar_x] .= nlc.nlp_hess_lag(z[1:nlc.nvar_x], obj_weight, y) # Original hessian
-	H[nlc.nvar_x+1:end, nlc.nvar_x+1:end] .= nlc.ρ * identity(nlc.nvar_r) # Added by residues (constant because of quadratic penalization)
-	return H
-end
-
-
-function NLPModels.hess_coord(nlc::NCLModel_, z::AbstractVector; obj_weight=1.0, y::AbstractVector)
+function NLPModels.hess_coord(nlc::NCLModel_, z::AbstractVector ; obj_weight=1.0, y=zeros)
 	increment!(nlc, :neval_hess)
 	rows, cols, vals = nlc.hess_coord(z[1:ncl.nvar_x], obj_weight, y)
 	rows = vcat(rows, ncl.nvar_x+1:ncl.nvar)
@@ -118,19 +127,21 @@ function NLPModels.hess_coord(nlc::NCLModel_, z::AbstractVector; obj_weight=1.0,
 	return (rows, cols, vals)
 end
 
-
-function NLPModels.hprod(nlc::NCLModel_, x::AbstractVector, v::AbstractVector; obj_weight=1.0, y)
+function NLPModels.hprod(nlc::NCLModel_, x::AbstractVector, v::AbstractVector ; obj_weight=1.0, y=zeros)
 	increment!(nlc, :neval_hprod)
 	nlp_Hv = 
 	Hv = zeros(nlc.nvar, 1)
-	Hv[1:ncl.nvar_x] = ncl.nlp_hprod(x, v[1:ncl.nvar_x]; obj_weight, y)
+	Hv[1:ncl.nvar_x] = ncl.nlp_hprod(x, v[1:ncl.nvar_x], obj_weight, y)
 	Hv[ncl.nvar_x+1:end] = ncl.ρ * v[ncl.nvar_x+1:end]
 	return Hv
 end
 
 
+
+
 function NLPModels.cons(nlc::NCLModel_, x::AbstractVector)
 	increment!(nlc, :neval_cons)
+	cx = 0
 	# Tri
 	# Standardisation + résidus
 	return cx
@@ -181,6 +192,9 @@ nlp = ADNLPModel(f, x0; lvar=lvar, uvar=uvar, c=c, lcon=lcon, ucon=ucon)
 
 
 
-ncl = NCLModel_(nlp, [0.0,0.0], 0.)
+ncl = NCLModel_(nlp, [0.,0.], 1., false)::NCLModel_
+
 x = ncl.meta.x0
-(obj(ncl, x), hess(ncl, x), grad(ncl, x))
+obj(ncl, x)
+hess(ncl, [0.,0.,0.], obj_weight = 1., y = [0.,0.])
+grad(ncl, x)
