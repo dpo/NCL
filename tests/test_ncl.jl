0a23b37a59117a5ee7df75624f984ebdb5d36881
Pierre-Elie <personnaz.pe@gmail.com>
2019-05-29 16:02:39 -0400

    Avancée, tests d'ipopt

--- tests/test_ncl.jl
+++ tests/test_ncl.jl
@@ -1,10 +1,52 @@
 using Test
 using NLPModels
+using Ipopt 
+using NLPModelsIpopt
 
 include("../src/ncl.jl")
+#include("../src/NLPModelsIpopt_perso.jl")
 
-function test_ncl(test::Bool) ::Test.DefaultTestSet
-    @testset "NCL algorithm" begin
-        @test true
+function test_ncl(test::Bool) #::Test.DefaultTestSet
+    # Test problem
+        ρ = 1.
+        y = [2., 1.]
+
+        f(x) = x[1] + x[2]
+        x0 = [1, 0.5]
+        lvar = [0., 0.]
+        uvar = [1., 1.]
+
+        lcon = [-0.5,
+                -1.,
+                -Inf,
+                0.5]
+        ucon = [Inf,
+                2.,
+                -0.5,
+                0.5]
+        c(x) = [x[1] - x[2], # linear
+                x[1]^2 + x[2], # non linear one, range constraint
+                x[1] - x[2], # linear, lower bounded 
+                x[1] * x[2]] # equality one
+
+        name = "Unitary test problem"
+        nlp = ADNLPModel(f, x0; lvar=lvar, uvar=uvar, c=c, lcon=lcon, ucon=ucon, name=name)::ADNLPModel
+        nlc = NLCModel(nlp, y, ρ)::NLCModel
+
+    resolution_k = ipopt(nlc)
+    x_k = resolution_k.solution # ! ordre des variables pas chamboulé ?
+    println(x_k)
+    println(cons(nlc, x_k))
+    # Get multipliers
+    # y_k = resolution_k.solver_specific[:multiplier_g]
+    # z_k_U = resolution_k.solver_specific[:multiplier_x_U]
+    # z_k_L = resolution_k.solver_specific[:multiplier_x_L]
+
+    if test
+        @testset "NCL algorithm" begin
+            @testset "NLPModel_solved() function" begin
+                @test NLPModel_solved(nlp, [1, 0.5], [00000000, 0., 0000000], [0000000,0.], [0.,0], 0.01) == true
+            end
+        end
     end
 end
