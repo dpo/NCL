e70f2407dd3193429eb9fd85754cc25289b13ac6
Pierre-Elie <personnaz.pe@gmail.com>
2019-05-29 16:02:49 -0400

    mise en forme

--- tests/test_main.jl
+++ tests/test_main.jl
@@ -5,12 +5,18 @@ using NLPModels
 include("test_ncl.jl")
 include("test_NLCModel.jl")
 
-function test_main(test_ncl_command::Bool, test_NCLModel_command::Bool, test_main_command::Bool) ::Test.DefaultTestSet
-    test_NLCModel(test_NCLModel_command)
-    test_ncl(test_ncl_command)
-    @testset "Main NCL" begin
-        @test true
+function test_main(test_NCLModel_command::Bool, test_ncl_command::Bool, test_main_command::Bool) #::Test.DefaultTestSet
+    if test_NCLModel_command
+        test_NLCModel(true)
+    end
+    if test_ncl_command
+        test_ncl(false)
+    end
+    if test_main_command
+        @testset "Main NCL" begin
+            @test true
+        end
     end
 end
 
-test_main(true,true,true)
+test_main(true,true,false)
