249f3b4fd8beb8228b82b99355c9a940455979bc
Pierre-Elie <personnaz.pe@gmail.com>
2019-05-30 18:01:56 -0400

    legers tests faits. Affichage À faire

--- src/main.jl
+++ src/main.jl
@@ -12,7 +12,7 @@ Main function for the NCL method.
     Calls ncl.jl on it,
     Returns (x (solution), y (lagrangian multipliers for constraints), z (lagrangian multpliers for bound constraints))
 """
-function NCLMain(nlp::AbstractNLPModel; tol = 0.01::Real, max_iter = 200::Int64, use_ipopt = true::Bool) ::Tuple{Vector{<:Real}, Vector{<:Real}, Vector{<:Real}} 
+function NCLMain(nlp::AbstractNLPModel; tol = 0.01::Real, max_iter = 200::Int64, use_ipopt = true::Bool, printing_iterations = false::Bool, printing_iterations_solver = false::Bool, printing_check = false::Bool) ::GenericExecutionStats
     if nlp.meta.ncon == 0
         if use_ipopt
             return ipopt(nlp, tol = tol, max_iter = max_iter)
@@ -21,5 +21,8 @@ function NCLMain(nlp::AbstractNLPModel; tol = 0.01::Real, max_iter = 200::Int64,
         end
     else
         nlc = NLCModel(nlp)
+        resol = ncl(nlc, max_iter, use_ipopt, tol, printing_iterations::Bool, printing_iterations_solver::Bool, printing_check::Bool)
+
+        return resol
     end
 end
--- tests/test_main.jl
+++ tests/test_main.jl
@@ -4,8 +4,9 @@ using NLPModels
 
 include("test_ncl.jl")
 include("test_NLCModel.jl")
+include("../src/main.jl")
 
-function test_main(test_NCLModel_command::Bool, test_ncl_command::Bool, test_main_command::Bool) #::Test.DefaultTestSet
+function test_main(test_NCLModel_command::Bool, test_ncl_command::Bool, test_main_command::Bool) ::Test.DefaultTestSet
     if test_NCLModel_command
         test_NLCModel(true)
     end
@@ -13,8 +14,33 @@ function test_main(test_NCLModel_command::Bool, test_ncl_command::Bool, test_mai
         test_ncl(true)
     end
     if test_main_command
-        @testset "Main NCL" begin
-            @test true
+        ρ = 1.
+        y = [2., 1.]
+
+        f(x) = x[1] + x[2]
+        x0 = [1, 0.5]
+        lvar = [0., 0.]
+        uvar = [1., 1.]
+
+        lcon = [-0.5,
+                -1.,
+                -Inf,
+                0.5]
+        ucon = [Inf,
+                2.,
+                0.5,
+                0.5]
+        c(x) = [x[1] - x[2], # linear
+                x[1]^2 + x[2], # non linear one, range constraint
+                x[1] - x[2], # linear, lower bounded 
+                x[1] * x[2]] # equality one
+
+        name = "Unitary test problem"
+        nlp = ADNLPModel(f, x0; lvar=lvar, uvar=uvar, c=c, lcon=lcon, ucon=ucon, name=name)::ADNLPModel
+
+        @testset "NCLMain" begin
+            @test isa(NCLMain(nlp), GenericExecutionStats)
+            @test NCLMain(nlp, max_iter = 15).iter <= 15
         end
     end
 end
