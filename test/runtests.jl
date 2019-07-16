79066708aff17ecb0931effd40cf101b3f247ec3
Pierre-Elie <personnaz.pe@gmail.com>
2019-07-16 10:58:29 -0400

    add KKTCheck test appart

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,12 +9,14 @@ using LinearAlgebra
 using SparseArrays
 using Test
 
-using NCL
+#using NCL
 
 include("../src/NCLModel.jl")
 include("test_NCLModel.jl")
 
-include("../src/KKT_check.jl")
+include("../src/KKTCheck.jl")
+include("test_KKTCheck.jl")
+
 include("../src/NCLSolve.jl")
 include("test_NCLSolve.jl")
 
@@ -24,11 +26,12 @@ include("test_NCLSolve.jl")
 # Run every test for NCL #
 ##########################
 """
-function test_main(test_NCLModel_command::Bool, test_NCLSolve_command::Bool) ::Test.DefaultTestSet
-    test_NLCModel(test_NCLModel_command)
+function test_main(test_NCLModel_command::Bool, test_KKTCheck_command::Bool, test_NCLSolve_command::Bool) ::Test.DefaultTestSet
+    test_NCLModel(test_NCLModel_command)
+    test_KKTCheck(test_KKTCheck_command)
     test_NCLSolve(test_NCLSolve_command)
 end
 ################################
 ################################
 
-test_main(false,true)
+test_main(true,true,true)
