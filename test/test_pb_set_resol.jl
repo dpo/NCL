84698b3459f6cd7cdfcc6358cae07b6236493c50
Pierre-Elie <personnaz.pe@gmail.com>
2019-07-31 16:10:20 -0400

    better tests names and file delete

--- test/test_pb_set_resol.jl
+++ test/test_pb_set_resol.jl
@@ -34,10 +34,10 @@ function test_pb_set_resol(test::Bool) ::Test.DefaultTestSet
     nlp = ADNLPModel(f, x0; lvar=lvar, uvar=uvar, c=c, lcon=lcon, ucon=ucon, name=name, lin = [1,3])::ADNLPModel
 
     @testset "pb_set_resol" begin
-        pb_set_resolution_data(cutest_pb_set = ["HS1", "HS10"], nlp_pb_set = [nlp], solver = ["nclres", "ipopt"])
-        @test isfile("./res/default_latex_table_name.tex")
-        rm("./res/default_latex_table_name.tex")
-        @test isfile("./res/default_profile_name.svg")
-        rm("./res/default_profile_name.svg")
+        pb_set_resolution_data(cutest_pb_set = ["HS1", "HS12"], nlp_pb_set = [nlp], solver = ["nclres", "ipopt"], create_latex_table=true, latex_table_name = "test_latex.tex", profile = true, profile_name = "test_profile.svg", )
+        @test isfile("./res/test_latex.tex")
+        isfile("./res/test_latex.tex") && rm("./res/test_latex.tex")
+        @test isfile("./res/test_profile.svg")
+        isfile("./res/test_profile.svg") && rm("./res/test_profile.svg")
     end
 end
